149a0a8e0e92d00d50de732a7a1f9b931c092e9a
akshats1 <akshats063@gmail.com>
2024-03-28 13:01:55 +0530

    cicd updated with cdcdyaml docker part uncommented

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-#from isd.pipeline.training_pipeline import TrainPipeline
-import os
-os.environ['WANDB_DISABLED'] = 'true'
 import sys,os
 from isd.pipeline.training_pipeline import TrainPipeline
 from isd.exception import isdException
 from isd.utils.main_utils import decodeImage, encodeImageIntoBase64
 from flask import Flask, request, jsonify, render_template,Response
 from flask_cors import CORS, cross_origin
-
+from isd.configuration.s3_operations import S3Operation
+from isd.entity.config_entity import ModelPusherConfig
 
 
 app = Flask(__name__)
@@ -17,7 +15,6 @@ CORS(app)
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
-
 
 
 @app.route("/train")
@@ -32,6 +29,33 @@ def trainRoute():
 def home():
     return render_template("index.html")
 
+def download_model_s3()-> str:
+    '''
+    Fetch data from s3
+    '''
+    try:
+        model_pusher_config = ModelPusherConfig()
+        s3 = S3Operation()
+    except Exception as e:
+        raise isdException(e, sys)
+
+    try: 
+        model_download_dir = "yolov7"+"/"+model_pusher_config.S3_MODEL_KEY_PATH
+        
+        if os.path.exists(model_download_dir):
+            print(f"File already exists: {model_download_dir}")
+        else:
+            # Download the file from S3 bucket
+            s3.download_object(
+                key= model_pusher_config.S3_MODEL_KEY_PATH, 
+                bucket_name= model_pusher_config.MODEL_BUCKET_NAME,
+                filename = model_download_dir
+            )
+            print(f"File downloaded successfully from S3 bucket: {model_pusher_config.MODEL_BUCKET_NAME}/{model_pusher_config.S3_MODEL_KEY_PATH}")
+        return model_pusher_config.S3_MODEL_KEY_PATH
+
+    except Exception as e:
+        raise isdException(e, sys)
 
 
 @app.route("/predict", methods=['POST','GET'])
@@ -41,10 +65,9 @@ def predictRoute():
         image = request.json['image']
         decodeImage(image, clApp.filename)
 
-       
-        os.system("cd yolov7/ && python detect.py --weights my_model.pt  --source ../data/inputImage.jpg")
-        #live image from camera
-        
+        model_file = download_model_s3()
+        print("downloaded model_file: ",model_file)
+        os.system("cd yolov7/ && python detect.py --weights best.pt  --source ../data/inputImage.jpg")
 
         opencodedbase64 = encodeImageIntoBase64("yolov7/runs/detect/exp/inputImage.jpg")
         result = {"image": opencodedbase64.decode('utf-8')}
